fe25d3fb2bb3ea303380f76647baeb3c0cb5fbed
Chriswkingg <chriswkingg@gmail.com>
2023-11-08 15:28:36 -0500

    Starting CLI, wip

--- DiffCopy/Program.cs
+++ DiffCopy/Program.cs
@@ -4,11 +4,40 @@ namespace DiffCopy;
 
 public static class Program
 {
+    public static CopyEngine ce;
+    public static FileList fl;
     public static void Main(string[] args)
     {
         Console.WriteLine("Enter source folder");
         var source = Console.ReadLine();
         Console.WriteLine("Enter dest folder");
         var dest = Console.ReadLine();
+        fl = new FileList();
+        fl.GenerateFileList(source);
+        ce = new()
+        {
+            FileList = null,
+            RootSrc = source,
+            RootDest = dest
+        };
+        StartConsole();
+    }
+
+    public static  void StartConsole()
+    {
+        while (true)
+        {
+            var line = Console.ReadLine();
+            if (line == null) return;
+            var parameters = line.Split();
+            switch (parameters[0])
+            {
+                case "start":
+                    break;
+                case "stop":
+                    break;
+            }
+            
+        }
     }
 }
